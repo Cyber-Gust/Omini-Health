55d42b7dd0c189c2f26b8a7389210498537be1b7
Cyber_Gust <augusto.e.silva@hotmail.com>
2025-09-16 12:53:03 -0300

    nmext

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
 
 const securityHeaders = [
   { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
-  { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+//  { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
 ];
 
 export default {
